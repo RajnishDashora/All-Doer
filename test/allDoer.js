945274ef9b2a7d3cfca7e7bd015b1271dedeb602
Rajnish <dashora.rajnish@gmail.com>
2014-12-17 18:04:53 +0530

    linear api ready

--- test/allDoer.js
+++ test/allDoer.js
@@ -1,7 +1,7 @@
 var should = require('chai').should();
 var alldoer= require('../lib/allDoer');
 describe('linearStructure',function(){
-  it('returns Hello',function(){
-    alldoer.linearStructure().should.equal('linearStructure');
+  it('creates directories',function(){
+    alldoer.linearStructure('/Users/rajnishdashora/work/nodejs/All-doer/lib/fixtures/',{"output":[1,2,3]});
   });
 });
