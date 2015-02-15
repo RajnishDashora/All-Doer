4d5d5a4a7fb1073a6ccd318bb0e5ba562aa2cd78
Rajnish <dashora.rajnish@gmail.com>
2015-02-15 19:16:53 +0530

    better spec

--- test/allDoer.js
+++ test/allDoer.js
@@ -2,24 +2,32 @@ var should = require('chai').should();
 var alldoer = require('../lib/allDoer');
 var rimraf = require('rimraf')
 var fs = require('fs');
+//variables used,
+var rootDirPath = '/Users/rajnishdashora/work/nodejs/All-doer/lib/fixtures/';
+var outputDirPath = rootDirPath + '/output';
+var firstPath = outputDirPath + '/first'
+var secondPath = outputDirPath + '/second'
+var thirdPath = outputDirPath + '/third'
+var structureJSON = {"output": ['first', 'second', 'third']};
 describe('linear one level structures', function () {
   beforeEach(function () {
-    rimraf.sync('/Users/rajnishdashora/work/nodejs/All-doer/lib/fixtures/output')
+    rimraf.sync(outputDirPath)
   });
   it('creates directory of directories', function () {
-    alldoer.linearDirOfFiles('/Users/rajnishdashora/work/nodejs/All-doer/lib/fixtures/', {"output": [1, 2, 3]});
-    result = fs.existsSync('/Users/rajnishdashora/work/nodejs/All-doer/lib/fixtures/output')
-    && fs.existsSync('/Users/rajnishdashora/work/nodejs/All-doer/lib/fixtures/output/1')
-    && fs.existsSync('/Users/rajnishdashora/work/nodejs/All-doer/lib/fixtures/output/2')
-    && fs.existsSync('/Users/rajnishdashora/work/nodejs/All-doer/lib/fixtures/output/3');
+
+    alldoer.linearDirOfFiles(rootDirPath , structureJSON );
+    result = fs.existsSync(outputDirPath)
+    && fs.existsSync(firstPath)
+    && fs.existsSync(secondPath)
+    && fs.existsSync(thirdPath);
     result.should.equal(true);
   });
   it('creates directory of files', function () {
-    alldoer.linearStructure('/Users/rajnishdashora/work/nodejs/All-doer/lib/fixtures/', {"output": [1, 2, 3]});
-    result = fs.existsSync('/Users/rajnishdashora/work/nodejs/All-doer/lib/fixtures/output')
-    && fs.existsSync('/Users/rajnishdashora/work/nodejs/All-doer/lib/fixtures/output/1')
-    && fs.existsSync('/Users/rajnishdashora/work/nodejs/All-doer/lib/fixtures/output/2')
-    && fs.existsSync('/Users/rajnishdashora/work/nodejs/All-doer/lib/fixtures/output/3');
+    alldoer.linearStructure(rootDirPath, structureJSON );
+    result = fs.existsSync(outputDirPath)
+    && fs.existsSync(firstPath)
+    && fs.existsSync(secondPath)
+    && fs.existsSync(thirdPath);
     result.should.equal(true);
   });
 });
